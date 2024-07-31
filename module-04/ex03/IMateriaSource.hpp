3e7c2993ed91d0efadc80eb372bd1e91abe84175
Gevorg Hovhannisyan <gehovhan@c2r3s4.42yerevan.am>
2024-07-31 21:03:29 +0400

    finish ex03 module-04

---
+++ module-04/ex03/IMateriaSource.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   IMateriaSource.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gehovhan <gehovhan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/31 20:52:46 by gehovhan          #+#    #+#             */
+/*   Updated: 2024/07/31 21:01:53 by gehovhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IMATERIASOURCE_HPP
+# define IMATERIASOURCE_HPP
+
+#include <iostream>
+#include "AMateria.hpp"
+
+class AMateria;
+
+class IMateriaSource {
+    public:
+        virtual ~IMateriaSource() {}
+        virtual void learnMateria(AMateria*) = 0;
+        virtual AMateria* createMateria(std::string const & type) = 0;
+};
+
+#endif
