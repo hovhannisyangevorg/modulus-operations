4d0e29f2bca6e61125d0163e39a923080dfe6cbe
Gevorg Hovhannisyan <gev9911@gmail.com>
2024-08-14 14:50:13 +0400

    [42Yerevan-02: clean-up] Clean up deprecated code and start new exercise ex00

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,9 +1,7 @@
-//
-// Created by Gevorg Hovhannisyan on 8/12/24.
-//
-
 #include <iostream>
+#include "Bureaucrat.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 int main() {
-    std::cout << "Hello ex02" << std::endl;
+
 }
