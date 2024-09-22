c367e24c4e72f6ed11e90f2d3f910e433ce2f715
Gevorg Hovhannisyan <gev9911@gmail.com>
2024-09-22 14:41:05 +0400

    [42Yerevan-00:patch] Module-06 Add Validator logics.

---
+++ CppModules/module-06/ex00/src/ScalaValidator.cpp
@@ -0,0 +1,27 @@
+#include <../incs/ScalaValidator.hpp>
+#include <../incs/Variable.hpp>
+#include <cctype>
+
+ScalaValidator::ScalaValidator() : variable() {}
+ScalaValidator::~ScalaValidator() {}
+ScalaValidator::ScalaValidator(const std::string& literal) : variable(literal) {}
+
+ScalaValidator::ScalaValidator(const ScalaValidator& v) : variable(v.variable) {}
+
+ScalaValidator& ScalaValidator::operator=(const ScalaValidator &)
+{
+    return *this;
+}
+
+void ScalaValidator::validate() {
+    this->variable.FindType();
+
+    if (this->variable.GetType() == UNKNOWN)
+        throw  std::runtime_error("ScalaValidator: Validation Failed.");
+}
+
+
+//ScalaValidator &ScalaValidator::operator=(const ScalarValidator &)
+//{
+//    return <#initializer#>;
+//}
