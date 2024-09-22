437fe108215bf605f05f96c3d2d187d131cb3d25
Gevorg Hovhannisyan <gev9911@gmail.com>
2024-09-22 14:40:05 +0400

    [42Yerevan-00:patch] Module-06 Add convertor functional.

--- CppModules/module-06/ex00/cmd/main.cpp
+++ CppModules/module-06/ex00/cmd/main.cpp
@@ -1,5 +1,22 @@
+#include <../incs/ScalarConverter.hpp>
+#include <string>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello module-06" << std::endl;
+int main(const int argc, const char** argv) {
+    std::string literal(argv[1]);
+
+    if (argc != 2) {
+        std::cout << "Error: Expected exactly one argument, but received " << (argc - 1) << std::endl;
+        std::cout << "Usage: " << argv[0] << " <argument>" << std::endl;
+        return 1;
+    }
+
+    try {
+        ScalarConverter::convert(literal);
+    }
+    catch (const ScalarConverter::ScalarConverterException& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
 }
